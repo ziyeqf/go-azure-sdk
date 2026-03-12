462f661b973086d438bfd658a8b82a4a8f1895fa
ziyeqf <51212351+ziyeqf@users.noreply.github.com>
2026-03-12 04:21:12 +0000

    add sql@2024-11-01-preview

---
+++ resource-manager/sql/2024-11-01-preview/instancepooloperations/model_instancepooloperation.go
@@ -0,0 +1,11 @@
+package instancepooloperations
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See NOTICE.txt in the project root for license information.
+
+type InstancePoolOperation struct {
+	Id         *string                          `json:"id,omitempty"`
+	Name       *string                          `json:"name,omitempty"`
+	Properties *InstancePoolOperationProperties `json:"properties,omitempty"`
+	Type       *string                          `json:"type,omitempty"`
+}
